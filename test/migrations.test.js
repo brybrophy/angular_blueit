44d546eb7d7d1d532e649c2203a003e8ae6f5f64
Bryan Brophy <brybrophy@gmail.com>
2016-07-26 18:08:39 -0700

    Write test for Posts Miration

--- test/migrations.test.js
+++ test/migrations.test.js
@@ -27,12 +27,97 @@ suite('Migrations', () => {
           nullable: false,
           defaultValue: 'nextval(\'topics_id_seq\'::regclass)'
         },
+
         name: {
           type: 'character varying',
           maxLength: 255,
           nullable: false,
           defaultValue: '\'\'::character varying'
         },
+
+        created_at: {
+          type: 'timestamp with time zone',
+          maxLength: null,
+          nullable: false,
+          defaultValue: 'now()'
+        },
+
+        updated_at: {
+          type: 'timestamp with time zone',
+          maxLength: null,
+          nullable: false,
+          defaultValue: 'now()'
+        }
+      };
+
+      for (const column in expected) {
+        assert.deepEqual(
+          actual[column],
+          expected[column],
+          `Column ${column} not the same`
+        );
+      }
+
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+  });
+
+  test('Posts Table', (done) => {
+    knex('posts').columnInfo()
+    .then((actual) => {
+      const expected = {
+        id: {
+          type: 'integer',
+          maxLength: null,
+          nullable: false,
+          defaultValue: 'nextval(\'posts_id_seq\'::regclass)'
+        },
+
+        title: {
+          type: 'character varying',
+          maxLength: 255,
+          nullable: false,
+          defaultValue: '\'\'::character varying'
+        },
+
+        image_url: {
+          type: 'character varying',
+          maxLength: 255,
+          nullable: false,
+          defaultValue: '\'\'::character varying'
+        },
+
+        description: {
+          type: 'text',
+          maxLength: null,
+          nullable: false,
+          defaultValue: '\'\'::text'
+        },
+
+        rating: {
+          type: 'integer',
+          maxLength: null,
+          nullable: false,
+          defaultValue: "0"
+        },
+
+        user_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+        topic_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
         created_at: {
           type: 'timestamp with time zone',
           maxLength: null,
