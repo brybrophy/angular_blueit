3c9b0bed83fa888c1c4772c2e8e8ab0a58404eda
Bryan Brophy <brybrophy@gmail.com>
2016-08-01 15:10:03 -0700

    Refactor controller to be split apart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
 
-  angular.module('blueitApp', []);
+  angular.module('blueitApp', ['angularMoment', 'ngRoute']);
 
 })();
--- js/controllers.js
+++ js/controllers.js
@@ -3,19 +3,14 @@
 
   const app = angular.module('blueitApp');
 
-  app.controller('TopicsCtrl', function() {
-    this.sortBy = ' ';
+  app.controller('TopicsCtrl', TopicsCtrl);
+  app.controller('PostsCtrl', PostsCtrl);
 
-    this.topics = [{
-    id: 1,
-    name: 'Dogs'
-    }, {
-    id: 2,
-    name: 'Cats'
-    }, {
-    id: 3,
-    name: 'Table Tennis'
-    }];
+  TopicsCtrl.$inject = ['$http'];
+  PostsCtrl.$inject = ['$http'];
+
+  function TopicsCtrl($http) {
+    this.sortBy = ' ';
 
     this.addTopic = () => {
       this.newTopic = {};
@@ -39,81 +34,9 @@
       this.topicsForm.topicName = '';
       this.newTopic = {};
     };
-  });
+  };
 
-  app.controller('PostsCtrl', ['$scope', function($scope) {
-    this.posts = [{
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 1,
-      "image_url": "https://b.thumbs.redditmedia.com/WTeAOJlQ98AfIVn7BjV_cDrDLXMuowwmJVo1p1xX5yg.jpg",
-      "rating": 6,
-      "title": "Dogs Are Not Allowed On NYC Subway Unless They're In A Carrier… So This Happened",
-      "topic_id": 1,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 2,
-      "image_url": "https://a.thumbs.redditmedia.com/pl1fM2jukfU2xW6hamMUF5dJ5gC_igj-1Z2oMwQM_90.jpg",
-      "rating": 2,
-      "title": "Wagging That Tail",
-      "topic_id": 1,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 3,
-      "image_url": "https://b.thumbs.redditmedia.com/9kZ1Hw7MF6zW41tvpivIswSVHzRehsP05friQEgaaYo.jpg",
-      "rating": -8,
-      "title": "My Friend Got a New Phone Case",
-      "topic_id": 2,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 4,
-      "image_url": "https://b.thumbs.redditmedia.com/AKy_8KjGjoeeq8CoYxjbWiFTsL2IR6Dh_VMbR1_ddnY.jpg",
-      "rating": 0,
-      "title": "Okay fine, you can have one more piece of chicken...",
-      "topic_id": 2,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 5,
-      "image_url": "https://b.thumbs.redditmedia.com/e-zhbAm5T-zELHrhRNYLV12l2fAgcqoM-yMF-IPUE6I.jpg",
-      "rating": 3,
-      "title": "The look of complete satisfaction",
-      "topic_id": 2,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 6,
-      "image_url": "http://www.toysrus.com/graphics/product_images/pTRU1-22957329v150.jpg",
-      "rating": -2,
-      "title": "Crazy Spin",
-      "topic_id": 3,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }, {
-      "created_at": new Date().getTime(),
-      "description": "What an awesome story.",
-      "id": 7,
-      "image_url": "http://blog.uberpong.com/wp-content/uploads/2012/11/Impossible-table-tennis-shot-dive-150x150.png",
-      "rating": 12,
-      "title": "Anyone here play in any clubs in Chicago?",
-      "topic_id": 3,
-      "updated_at": new Date().getTime(),
-      "user_id": 1
-    }];
-
+  function PostsCtrl($http) {
     this.addPost = () => {
       this.newPost = {};
 
@@ -151,5 +74,5 @@
     this.downVote = (post) => {
       post.rating -= 1;
     };
-  }]);
+  };
 })();
