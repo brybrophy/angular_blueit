d6d44938bcaf7c0563076dacb51adacca218fe7e
Bryan Brophy <brybrophy@gmail.com>
2016-08-05 16:11:28 -0700

    Split Aside Forms into directives and re-style

---
+++ public/js/directives/register.directive.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.directive('bbRegisterForm', function() {
+  return {
+    templateUrl: 'components/register-form.html'
+  };
+});
---
+++ public/js/directives/topics.directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+app.directive('bbTopicsForm', function() {
+  return {
+    templateUrl: 'components/topics-form.html'
+  }
+});
+
+app.directive('bbTopicsList', function() {
+  return {
+    templateUrl: 'components/topics-list.html'
+  }
+});
