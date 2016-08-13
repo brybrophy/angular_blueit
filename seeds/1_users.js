92cd9c28334a9972f2a5e55437b0ca059d53748d
Bryan Brophy <brybrophy@gmail.com>
2016-08-12 22:58:28 -0700

    Import logos and favicon

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       return knex('users').insert([{
         id: 1,
         username: 'admin',
-        email: 'admin@blueit.com',
+        email: 'admin@workflow.com',
         hashed_password: '$2a$12$Q3fh1jeJZ2Q19Yr12aVOxO54a/IvBhS01qWCqxNAZc0ABRxq0NnYq',
         created_at: new Date('2016-07-23 14:26:16 UTC'),
         updated_at: new Date('2016-07-23 14:26:16 UTC')
